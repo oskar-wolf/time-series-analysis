224e0a49f551c25aab9ddccc2df544c4ad438c37
Oskar Wolf <mroskarwolf97@gmail.com>
2024-10-25 10:56:56 +0200

    advanced topics

--- Unit_6_Advanced_Topics/6_1_BATS_TBATS.py
+++ Unit_6_Advanced_Topics/6_1_BATS_TBATS.py
@@ -13,17 +13,15 @@ import numpy as np
 import pickle
 
 # %% load and prepare the sample data
-df = pd.read_csv('AEP_hourly_full.csv', sep=',')
-df = df[(df.Datetime >= '2011-02-01 00:00:00') & \
-    (df.Datetime < '2011-05-21 00:00:00')]
-df = df.rename(columns={'AEP_MW': 'energy'})
-df.index = pd.to_datetime(df.Datetime, dayfirst=True, \
-    infer_datetime_format=True)
+df = pd.read_csv("AEP_hourly_full.csv", sep=",")
+df = df[(df.Datetime >= "2011-02-01 00:00:00") & (df.Datetime < "2011-05-21 00:00:00")]
+df = df.rename(columns={"AEP_MW": "energy"})
+df.index = pd.to_datetime(df.Datetime, dayfirst=True, infer_datetime_format=True)
 df = df.sort_index()
 
 # %% split the data
-X_train = df.iloc[:-(20*24)].energy
-X_test = df.iloc[-(20*24):].energy
+X_train = df.iloc[: -(20 * 24)].energy
+X_test = df.iloc[-(20 * 24) :].energy
 
 ## BATS
 
@@ -31,17 +29,17 @@ X_test = df.iloc[-(20*24):].energy
 bats_estimator = BATS(
     seasonal_periods=[24, 169],
     use_arma_errors=True,  # consider ARMA
-    use_box_cox=True, # consider Box-Cox Trans.
-    use_damped_trend=False, # no damping 
-    use_trend = False, # no trend
-    n_jobs=1 # number of parallel jobs
+    use_box_cox=True,  # consider Box-Cox Trans.
+    use_damped_trend=False,  # no damping
+    use_trend=False,  # no trend
+    n_jobs=1,  # number of parallel jobs
 )
 
 # %% fit the model
 bats_model = bats_estimator.fit(X_train)
 
 # %% write object to file
-with open('bats_model.pkl', 'wb') as f:
+with open("bats_model.pkl", "wb") as f:
     pickle.dump(bats_model, f)
 
 # %% load object from file
@@ -52,58 +50,52 @@ with open('bats_model.pkl', 'wb') as f:
 print(bats_model.summary())
 
 # %% extract fitted values and add to Data Frame
-bats_fit = pd.DataFrame(bats_model.y_hat, \
-    columns=['bats_fit'], index=df.index[:len(X_train)])
-df['bats_fit'] = bats_fit
+bats_fit = pd.DataFrame(
+    bats_model.y_hat, columns=["bats_fit"], index=df.index[: len(X_train)]
+)
+df["bats_fit"] = bats_fit
 
 # %% use the model to forecast
 fc = bats_model.forecast(steps=len(X_test))
-fc = pd.DataFrame(fc, columns=['bats_forecast'], \
-    index=df.index[len(X_train):])
-df['bats_forecast'] = fc
+fc = pd.DataFrame(fc, columns=["bats_forecast"], index=df.index[len(X_train) :])
+df["bats_forecast"] = fc
 
 # %% plot the fitted/forecasted data
-plt.rcParams.update({'font.size': 14})
-fig, (ax1, ax2) = plt.subplots(2, \
-    figsize=(16, 10), dpi=100)
+plt.rcParams.update({"font.size": 14})
+fig, (ax1, ax2) = plt.subplots(2, figsize=(16, 10), dpi=100)
 
 # observed data
-ax1.plot(df.index, df.energy, \
-    color='k', label='Actual')
+ax1.plot(df.index, df.energy, color="k", label="Actual")
 
 # fitted values
-ax1.plot(df.index, df.bats_fit, \
-    color='b', label='Fitted')
+ax1.plot(df.index, df.bats_fit, color="b", label="Fitted")
 
 # forecasted values
-ax1.plot(df.index, df.bats_forecast, \
-    color='r', label='Forecast')
+ax1.plot(df.index, df.bats_forecast, color="r", label="Forecast")
 
 # set artists
-ax1.set_xlabel('Date')
-ax1.set_ylabel('Consumption (MW)')
+ax1.set_xlabel("Date")
+ax1.set_ylabel("Consumption (MW)")
 ax1.grid()
 ax1.legend()
 
 # second plot - zoom to 10 days
-df_zoom = df[(df.Datetime >= '2011-04-20 00:00:00') & \
-    (df.Datetime < '2011-05-10 00:00:00')]
+df_zoom = df[
+    (df.Datetime >= "2011-04-20 00:00:00") & (df.Datetime < "2011-05-10 00:00:00")
+]
 
 # observed data
-ax2.plot(df_zoom.index, df_zoom.energy, \
-    color='k', label='Actual')
+ax2.plot(df_zoom.index, df_zoom.energy, color="k", label="Actual")
 
 # fitted values
-ax2.plot(df_zoom.index, df_zoom.bats_fit, \
-    color='b', label='Fitted')
+ax2.plot(df_zoom.index, df_zoom.bats_fit, color="b", label="Fitted")
 
 # forecasted values
-ax2.plot(df_zoom.index, df_zoom.bats_forecast, \
-    color='r', label='Forecast')
+ax2.plot(df_zoom.index, df_zoom.bats_forecast, color="r", label="Forecast")
 
 # set artists
-ax1.set_xlabel('Date')
-ax1.set_ylabel('Consumption (MW)')
+ax1.set_xlabel("Date")
+ax1.set_ylabel("Consumption (MW)")
 ax2.grid()
 
 plt.show()
@@ -115,14 +107,14 @@ tbats_estimator = TBATS(
     seasonal_periods=[24, 169],
     use_arma_errors=True,  # consider ARMA
     use_box_cox=True,  # consider Box-Cox transformation
-    use_damped_trend=False, # no damping
-    use_trend = False, # no trend
-    n_jobs=1 # number of parallel jobs
+    use_damped_trend=False,  # no damping
+    use_trend=False,  # no trend
+    n_jobs=1,  # number of parallel jobs
 )
 tbats_model = tbats_estimator.fit(X_train)
 
 # %% write object to file
-with open('tbats_model.pkl', 'wb') as f:
+with open("tbats_model.pkl", "wb") as f:
     pickle.dump(tbats_model, f)
 
 # %% load object from file
@@ -133,58 +125,52 @@ with open('tbats_model.pkl', 'wb') as f:
 print(tbats_model.summary())
 
 # %% extract fitted values and add to Data Frame
-tbats_fit = pd.DataFrame(tbats_model.y_hat, \
-    columns=['tbats_fit'], index=df.index[:len(X_train)])
-df['tbats_fit'] = tbats_fit
+tbats_fit = pd.DataFrame(
+    tbats_model.y_hat, columns=["tbats_fit"], index=df.index[: len(X_train)]
+)
+df["tbats_fit"] = tbats_fit
 
 # %% use the model to forecast
 fc = tbats_model.forecast(steps=len(X_test))
-fc = pd.DataFrame(fc, columns=['tbats_forecast'], \
-    index=df.index[len(X_train):])
-df['tbats_forecast'] = fc
+fc = pd.DataFrame(fc, columns=["tbats_forecast"], index=df.index[len(X_train) :])
+df["tbats_forecast"] = fc
 
 # %% plot the fitted/forecasted data
-plt.rcParams.update({'font.size': 14})
-fig, (ax1, ax2) = plt.subplots(2, \
-    figsize=(16, 10), dpi=100)
+plt.rcParams.update({"font.size": 14})
+fig, (ax1, ax2) = plt.subplots(2, figsize=(16, 10), dpi=100)
 
 # observed data
-ax1.plot(df.index, df.energy, \
-    color='k', label='Actual')
+ax1.plot(df.index, df.energy, color="k", label="Actual")
 
 # fitted values
-ax1.plot(df.index, df.tbats_fit, \
-    color='b', label='Fitted')
+ax1.plot(df.index, df.tbats_fit, color="b", label="Fitted")
 
 # forecasted values
-ax1.plot(df.index, df.tbats_forecast, \
-    color='r', label='Forecast')
+ax1.plot(df.index, df.tbats_forecast, color="r", label="Forecast")
 
 # set artists
-ax1.set_xlabel('Date')
-ax1.set_ylabel('Consumption (MW)')
+ax1.set_xlabel("Date")
+ax1.set_ylabel("Consumption (MW)")
 ax1.grid()
 ax1.legend()
 
 # second plot - zoom to 10 days
-df_zoom = df[(df.Datetime >= '2011-04-20 00:00:00') & \
-    (df.Datetime < '2011-05-10 00:00:00')]
+df_zoom = df[
+    (df.Datetime >= "2011-04-20 00:00:00") & (df.Datetime < "2011-05-10 00:00:00")
+]
 
 # observed data
-ax2.plot(df_zoom.index, df_zoom.energy, \
-    color='k', label='Actual')
+ax2.plot(df_zoom.index, df_zoom.energy, color="k", label="Actual")
 
 # fitted values
-ax2.plot(df_zoom.index, df_zoom.tbats_fit, \
-    color='b', label='Fitted')
+ax2.plot(df_zoom.index, df_zoom.tbats_fit, color="b", label="Fitted")
 
 # forecasted values
-ax2.plot(df_zoom.index, df_zoom.tbats_forecast, \
-    color='r', label='Forecast')
+ax2.plot(df_zoom.index, df_zoom.tbats_forecast, color="r", label="Forecast")
 
 # set artists
-ax1.set_xlabel('Date')
-ax1.set_ylabel('Consumption (MW)')
+ax1.set_xlabel("Date")
+ax1.set_ylabel("Consumption (MW)")
 ax2.grid()
 plt.show()
 # %%
